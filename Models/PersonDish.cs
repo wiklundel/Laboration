758b40b4334860a4face1cd9a4135732b7737a57
Elvira Wiklund <wiklundel@gmail.com>
2025-11-14 13:09:24 +0100

    Skapat konstruktor

---
+++ Models/PersonDish.cs
@@ -0,0 +1,21 @@
+public class PersonDish
+{
+    [Key]
+    public int personDishID { get; set; }
+    public int dishID { get; set; }
+    public Dish? Dish { get; set; }
+    public int personID { get; set; }
+    public Person? Person { get; set; }
+
+    public PersonDish()
+    {
+        
+    }
+
+    public PersonDish(int PersonDishID, int personID, int dishID)
+    {
+        this.personDishID = personDishID;
+        this.personID = personID;
+        this.dishID = dishID;
+    }
+}
