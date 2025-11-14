dead8d3255cc9b54cb846ae4c344212afae8bcfb
Elvira Wiklund <wiklundel@gmail.com>
2025-11-14 12:48:46 +0100

    Påbörjat Dish

--- Models/Dish.cs
+++ Models/Dish.cs
@@ -1,2 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 
+public class Dish
+{
+    [Key]
+    public int DishID {get; set;}
 
+    [Required, StringLength(80)]
+    public string DishName {get; set;}
+
+    public List<PersonDish>? PersonDishes {get; set;}
+}
