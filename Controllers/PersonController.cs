fcf0bef043a9d6c4f183b30a96e92e0a2f7cd559
Elvira Wiklund <wiklundel@gmail.com>
2025-11-19 18:43:09 +0100

    skapat första .cshtml filen:)

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Laboration.Models;
 
-namespace LABORATION.Controllers
+namespace Laboration.Controllers
 {
     public class PersonController : Controller
     {
