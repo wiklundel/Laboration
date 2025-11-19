3ea662620a0044e2acca22b799fd96382ae31639
Elvira Wiklund <wiklundel@gmail.com>
2025-11-19 18:30:53 +0100

    Skapart första controllern

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LABORATION.Controllers
+{
+    public class PersonController : Controller
+    {
+        static IList<Person> personList = new List<Person>
+        {
+            new Person() { FirstName = "Alice", LastName = "Andersson", Age = 30 },
+            new Person() { FirstName = "Bob", LastName = "Bengtsson", Age = 25 },
+            new Person() { FirstName = "Charlie", LastName = "Carlsson", Age = 35 }
+        };
+
+        // GET: Person
+        public IActionResult Persons()
+        {
+            return View(personList);    
+        }
+    }
+}
