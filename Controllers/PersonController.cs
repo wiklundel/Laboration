f5d02b55fcead7708c8e885c76bae39d72797fec
Elvira Wiklund <wiklundel@gmail.com>
2025-11-20 09:43:32 +0100

    en view är klar

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -1,21 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Laboration.Models;
 
-namespace Laboration.Controllers
-{
-    public class PersonController : Controller
-    {
-        static IList<Person> personList = new List<Person>
-        {
-            new Person() { FirstName = "Alice", LastName = "Andersson", Age = 30 },
-            new Person() { FirstName = "Bob", LastName = "Bengtsson", Age = 25 },
-            new Person() { FirstName = "Charlie", LastName = "Carlsson", Age = 35 }
-        };
+namespace Laboration.Controllers;
 
-        // GET: Person
-        public IActionResult Persons()
-        {
-            return View(personList);    
-        }
+public class PersonController : Controller
+{
+    static IList<Person> personList = new List<Person>
+    {
+        new Person() { FirstName = "Alice", LastName = "Andersson", Age = 30 },
+        new Person() { FirstName = "Bob", LastName = "Bengtsson", Age = 25 },
+        new Person() { FirstName = "Charlie", LastName = "Carlsson", Age = 35 }
+    };
+
+    // GET: Person
+    public IActionResult Persons()
+    {
+        return View(personList);    
     }
 }
